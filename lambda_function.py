36c79dbba8d11d47ddbe74e1468a1f3473414b2c
Hiroki Okui <hiroki.okui@gmail.com>
2021-03-20 15:13:15 +0900

    update

--- lambda_function.py
+++ lambda_function.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     print(requests.__version__)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello world from GitHub Actions!!')
+        'body': json.dumps('Hello world from New Env!!')
     }
